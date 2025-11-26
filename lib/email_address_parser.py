4588c6142bf113e5e35d546119b1a71b06753b74
SylviaMalala <sylviamalala17@gmail.com>
2025-11-26 16:32:32 +0300

    email

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,11 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+    
+    def parse(self):
+        tokens = re.split(r'[,\s]+', self.email_string)
+        email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        valid_emails = [token for token in tokens if re.match(email_pattern, token)]
+        return sorted(set(valid_emails))
